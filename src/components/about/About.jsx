af2c64f43fcf71514e3c29c8e8e836e98c47f6ec
Logan Scott <logantscott@gmail.com>
2020-08-06 19:12:05 -0600

    developers component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import styles from './About.css';
+import Developer from '../developers/Developers';
 
 const About = () => {
 
   return (
     <main className={styles.About}>
-      <figure>
-        <img alt='nikki' src='..\..\public\images\nikki.jfif' />
-        <figcaption>
-          <p className={styles.name}>Nikki Kiga</p>
-          <p>Software Developer</p>
-          <a href='https://github.com/nikki-kiga'>Github</a>
-        </figcaption>
-      </figure>
-      <figure>
-        <img alt='max' src='..\..\public\images\max.jfif' />
-        <figcaption>
-          <p className={styles.name}>Max Lamb</p>
-          <p>Software Developer</p>
-          <a href='https://github.com/MaximusLamb'>Github</a>
-        </figcaption>
-      </figure>
-      <figure>
-        <img alt='logan' src='..\..\public\images\logan.jfif' />
-        <figcaption>
-          <p className={styles.name}>Logan Scott</p>
-          <p>Full-Stack Developer</p>
-          <a href='https://github.com/logantscott'>Github</a>
-        </figcaption>
-      </figure>
+      <Developer />
     </main>
   );
 };
